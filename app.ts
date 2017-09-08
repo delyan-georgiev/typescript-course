465714dfb5ce6f567dfc9a232bc5811d1340502e
Delyan Georgiev <delyan4520@gmail.com>
2017-09-08 19:27:01 +0300

    initial commit

---
+++ app.ts
@@ -0,0 +1,20 @@
+type BankAccount = {
+    money: number,
+    deposit: (value: number) => void
+}
+let bankAccount: BankAccount = {
+    money: 2000,
+    deposit(value: number): void {
+        this.money += value;
+    }
+};
+
+let myself: {name: string, bankAccount: BankAccount, hobbies: string[] | number[]} = {
+    name: 'Cecka',
+    bankAccount: bankAccount,
+    hobbies: ['Sports', 'Cooking', 7]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
